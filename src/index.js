0e4b9aab2023545395abdb1c16248151543048d9
Martin Bielik <mx.bielik@gmail.com>
2022-12-13 17:25:21 +0100

    updated module resolution config

--- src/index.js
+++
@@ -1,4 +0,0 @@
-export * from 'amazon-cognito-identity-js';
-// overrides exports from amazon-cognito-identity-js
-export { CognitoUser } from './CognitoUser';
-export { CognitoUserPool } from './CognitoUserPool';
